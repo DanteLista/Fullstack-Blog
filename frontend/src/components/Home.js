4b568a693c96da0e0b33207b6e95034a6a979354
aalighierii <84481705+aalighierii@users.noreply.github.com>
2021-11-07 01:04:05 -0400

    complete .

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -13,10 +13,10 @@ const home = () => (
 <div className="container">
 <div className="jumbotron mt-5">
     <h1 className="display-4">Welcome to my Blog!</h1>
-    <p className="lead">Stay a while, and check out some cool stuff.  :)</p>
+    <p className="lead">This Blog is a Django and React project. I hope you enjoy the content!</p>
     <hr className="my-4"/>
-    <p>Click the button below to check out some sick posts.</p>
-    <Link className="btn btn-primary btn-lg" to="/blog" role="button">To the Blog!</Link>
+    <p>Click the button below to check out some cool posts :) </p>
+    <Link className="btn btn-primary btn-lg" to="/blog" role="button">Click Here!</Link>
   </div>
   </div>
 );
